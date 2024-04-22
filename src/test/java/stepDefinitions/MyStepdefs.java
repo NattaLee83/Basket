3b79701f01d8ea85cdfc2226bc252e77268e1618
NattaLee83 <nattalee83@hotmail.com>
2024-04-22 15:29:14 +0200

    ok

--- src/test/java/stepDefinitions/MyStepdefs.java
+++ src/test/java/stepDefinitions/MyStepdefs.java
@@ -1,2 +1,128 @@
-package stepDefinitions;public class MyStepdefs {
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class MyStepdefs {
+
+    private WebDriver driver; //Attribut
+
+    private void waitForElement(By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+    @Given("I am using {} as browser")
+    public void iAmUsingAsBrowser(String browser) {
+
+        if(browser.equals("firefox")){
+            driver = new FirefoxDriver();
+        }else {
+            driver = new ChromeDriver();
+        }
+    }
+
+    @Given("I have entered date of birth {string}")
+    public void iHaveEnteredDateOfBirth(String date) {
+        driver.get("https://membership.basketballengland.co.uk/NewSupporterAccount");
+        driver.manage().window().setSize(new Dimension(1710, 983));
+        driver.findElement(By.id("dp")).sendKeys(date);
+    }
+
+    @And("I have entered first name {string}")
+    public void iHaveEnteredFirstName(String firstname) {
+        waitForElement(By.id("member_firstname"));
+        driver.findElement(By.id("member_firstname")).sendKeys(firstname);
+    }
+
+    @And("I have entered last name {string}")
+    public void iHaveEnteredLastName(String lastname) {
+        driver.findElement(By.id("member_lastname")).sendKeys(lastname);
+    }
+
+    @And("I have entered email and confirmed email {string}")
+    public void iHaveEnteredEmailAndConfirmedEmail(String email) {
+        driver.findElement(By.id("member_emailaddress")).sendKeys(email);
+        driver.findElement(By.id("member_confirmemailaddress")).sendKeys(email);
+    }
+
+    @And("I have entered password {string}")
+    public void iHaveEnteredPassword(String password) {
+        driver.findElement(By.id("signupunlicenced_password")).sendKeys(password);
+
+    }
+    @And("I have {string} password")
+    public void iHavePassword(String confirmed) {
+        driver.findElement(By.id("signupunlicenced_confirmpassword")).sendKeys(confirmed);
+    }
+
+    @And("I have checked terms and conditions")
+    public void iHaveCheckedTermsAndConditions() {
+        driver.findElement(By.cssSelector(".md-checkbox > .md-checkbox:nth-child(1) .box")).click();
+
+    }
+
+    @And("I have checked Over 18 and code of conduct")
+    public void iHaveCheckedOverAndCodeOfConduct() {
+        driver.findElement(By.cssSelector(".md-checkbox:nth-child(2) > label > .box")).click();
+        driver.findElement(By.cssSelector(".md-checkbox:nth-child(7) .box")).click();
+    }
+
+    @When("I press the join button")
+    public void iPressTheJoinButton() throws InterruptedException {
+        waitForElement(By.name("join"));
+        driver.findElement(By.name("join")).click();
+    }
+
+    @Then("I am registered and verifies registered")
+    public void iAmRegisteredAndVerifiesRegistered() {
+        String expected = "THANK YOU FOR CREATING AN ACCOUNT WITH BASKETBALL ENGLAND"; //Verifiera en text.
+        String actual = driver.findElement(By.cssSelector("body > div > div.page-content-wrapper > div > h2")).getText();
+        assertEquals(expected, actual);
+    }
+
+    @Then("I am registered and verifies missing lastname")
+    public void iAmRegisteredAndVerifiesMissingLastname() {
+        String expected = "Last Name is required";
+        String actual = driver.findElement(By.cssSelector("#signup_form > div:nth-child(6) > div:nth-child(2) > div > span > span")).getText();
+        assertEquals(expected, actual);                 //#signup_form > div:nth-child(6) > div:nth-child(2) > div > span > span
+    }
+
+    @Then("I am registered and verifies wrong password")
+    public void iAmRegisteredAndVerifiesWrongPassword() {
+        String expected = "Password did not match";
+        String actual = driver.findElement(By.cssSelector(".row:nth-child(2) .warning > span")).getText();
+        assertEquals(expected, actual);
+
+    }
+
+    @Then("I have not checked terms and conditions")
+    public void iHaveNotCheckedTermsAndConditions() {
+
+    }
+
+    @Then("I am registered and verifies terms and conditions")
+    public void iAmRegisteredAndVerifiesTermsAndConditions() {
+        String expected = "You must confirm that you have read and accepted our Terms and Conditions";
+        String actual = driver.findElement(By.cssSelector("#signup_form > div:nth-child(12) > div > div:nth-child(2) > div:nth-child(1) > span > span")).getText();
+        assertEquals(expected, actual);
+
+    }
 }
+
+
+
--- src/test/resources/features/Registration.feature
+++ src/test/resources/features/Registration.feature
@@ -1,56 +1,24 @@
 Feature: Registration
 
-  Scenario: Successful registration
-    Given I have entered date of birth "01/01/2001
-    And I have entered first name "Natta"
-    And I have entered last name "Lee"
-    And I have entered email and confirmed email "nattalee@gmail.com"
-    And I have entered password and confirmed password
-    And I have checked terms and conditions
+  Scenario Outline:
+    Given I am using <browser> as browser
+    Given I have entered date of birth "<date>"
+    And I have entered first name "<firstname>"
+    And I have entered last name "<lastname>"
+    And I have entered email and confirmed email "<email>"
+    And I have entered password "<password>"
+    And I have "<confirmed>" password
+    And I have <status> terms and conditions
     And I have checked Over 18 and code of conduct
     When I press the join button
-    Then I am registrered
+    Then I am registered and verifies <text>
 
-  Scenario: Missing surname
-    And I have entered first name "Natta"
-    And I have entered last name ""
-    And I have entered email and confirmed email "nattalee@gmail.com"
-    And I have entered password and confirmed password
-    And I have checked terms and conditions
-    And I have checked Over 18 and code of conduct
-    When I press the join button
-    Then I am registrered
-
-  Scenario: Password does not match
-    And I have entered first name "Natta"
-    And I have entered last name "Lee"
-    And I have entered email and confirmed email "nattalee@gmail.com"
-    And I have entered password and confirmed password
-    And I have checked terms and conditions
-    And I have checked Over 18 and code of conduct
-    When I press the join button
-    Then I am registrered
-
-  Scenario: Terms and conditions are not approved
-    And I have entered first name "Natta"
-    And I have entered last name "Lee"
-    And I have entered email and confirmed email "nattalee@gmail.com"
-    And I have entered password and confirmed password
-    And I have checked terms and conditions
-    And I have checked Over 18 and code of conduct
-    When I press the join button
-    Then I am registrered
-
-
-  #Scenario Outline:
-  #Given
-  #When
-  #Then
-
-
-    #Examples:
-      #| user | value | status |
-      #|  |  | fail |
+    Examples:
+      | browser | date       |  | firstname | lastname | email                    | password   | confirmed  | status      | text                 |
+      | firefox | 01/01/2001 |  | Natta     | Lee      | nattalee456789@gmail.com | nattalee01 | nattalee01 | checked     | registered           |
+      | chrome  | 02/02/2002 |  | Nova      |          | nova16@gmail.com         | novalito16 | novalito16 | checked     | missing lastname     |
+      | chrome  | 03/03/2003 |  | Mila      | Mila     | mila22@gmail.com         | milamila22 | milamila2  | checked     | wrong password       |
+      | firefox | 04/04/2004 |  | Zoran     | Zoran    | zoran83@gmail.com        | zoran83    | zoran83    | not checked | terms and conditions |
 
 
 
